b2a79f4cab144dd34487268e3d4f13b83be49ad3
Tychon <tisha.kuprevich@gmail.com>
2018-02-13 19:27:00 +0300

    Data binding with lowercase filter

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -2,14 +2,20 @@
 var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', '$timeout', function ($scope, $timeout) {
+angularApp.controller('mainController', ['$scope', '$timeout', '$filter', function ($scope, $timeout, $filter) {
 
     $scope.name = 'Tisha';
 
-    $timeout(function() {
+    $timeout(function () {
 
         $scope.name = 'Everybody';
 
     }, 3000);
+
+    $scope.handle = '';
+
+    $scope.lowercasehandle = function () {
+        return $filter('lowercase')($scope.handle);
+    }
 
 }]);
