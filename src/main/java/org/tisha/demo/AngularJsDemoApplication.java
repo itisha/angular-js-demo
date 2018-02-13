a6587a556dd44352f93cbdb2ed58802074420d17
Tychon <tisha.kuprevich@gmail.com>
2018-02-13 19:10:43 +0300

    Initial commit with directives

---
+++ src/main/java/org/tisha/demo/AngularJsDemoApplication.java
@@ -0,0 +1,12 @@
+package org.tisha.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularJsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularJsDemoApplication.class, args);
+	}
+}
---
+++ src/main/resources/static/app.js
@@ -0,0 +1,15 @@
+// MODULE
+var angularApp = angular.module('angularApp', []);
+
+// CONTROLLERS
+angularApp.controller('mainController', ['$scope', '$timeout', function ($scope, $timeout) {
+
+    $scope.name = 'Tisha';
+
+    $timeout(function() {
+
+        $scope.name = 'Everybody';
+
+    }, 3000);
+
+}]);
