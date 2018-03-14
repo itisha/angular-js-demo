3ea666e26e93a9530c6eac73404f2ce6806d77b5
Tychon <tisha.kuprevich@gmail.com>
2018-03-15 00:04:25 +0300

    Directive link function little demo

--- src/main/resources/static/directives.js
+++ src/main/resources/static/directives.js
@@ -18,3 +18,20 @@ angularApp.directive('isolatedScopeObject', function () {
         'Change</button>'
     };
 });
+
+angularApp.directive('linkDemo', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, elem, attrs) {
+            elem.on('click', function () {
+                elem.html('You clicked me');
+            });
+            elem.on('mouseenter', function () {
+                elem.css('background-color', 'yellow');
+            });
+            elem.on('mouseleave', function () {
+                elem.css('background-color', 'white');
+            });
+        }
+    };
+});
