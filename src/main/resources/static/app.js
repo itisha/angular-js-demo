f3998c02ff15ac349ef9cdda63bcd29412bc7799
Tychon <tisha.kuprevich@gmail.com>
2018-03-18 21:45:35 +0300

    Table helper (accessible only on a separate page)

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -57,7 +57,30 @@ angularApp.controller('directivesController', function ($scope) {
     };
     $scope.func = function () {
         console.log('action called');
-    }
+    };
+    $scope.customers = [
+        {
+            "name": "James",
+            "last_name": "Butterburg",
+            "age": 31,
+            "street": "Chapaeva 4",
+            "url": "http://ya.ru"
+        },
+        {
+            "name": "Josephine",
+            "last_name": "Darakjy",
+            "age": 36,
+            "street": "Chapaeva 2",
+            "url": "http://ya.ru/1"
+        },
+        {
+            "name": "Art",
+            "last_name": "Chemel",
+            "age": 33,
+            "street": "Chapaeva 1",
+            "url": "http://ya.ru/2"
+        }
+    ]
 });
 
 angularApp.controller('testController', ['$scope', '$timeout', '$filter', '$http', '$location', function ($scope, $timeout, $filter, $http, $location) {
